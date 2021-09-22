0449986226c341d0cb8ca8f1b3493de280c3050d
Szymon <Szymon@LAPTOP-2DIS65OF>
2021-09-22 12:17:33 +0200

    Add project files.

---
+++ tydzien2_lekcja7_zadanie13/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace tydzien2_lekcja7_zadanie13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj pierwszą liczbę");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj drugą liczbę");
+            int b = int.Parse(Console.ReadLine());
+
+            double dodawanie = (a + b);
+            double odejmowanie = (a - b);
+            double mnożenie = (a * b);
+            double dzielenie = (a / b);
+
+            Console.WriteLine("Wybierz opcję");
+            Console.WriteLine("1 - dodawanie");
+            Console.WriteLine("2 - odejmowanie");
+            Console.WriteLine("3 - mnożenie");
+            Console.WriteLine("4 - dzielenie");
+
+            int działanie = int.Parse(Console.ReadLine());
+
+            if (działanie == 1)
+                Console.WriteLine($"Twój wynik dodawania to: {dodawanie}");
+            if (działanie == 2)
+                Console.WriteLine($" Twój wynik to: {odejmowanie}");
+            if (działanie == 3)
+                Console.WriteLine($"Twój wynik to: {mnożenie}");
+            if (działanie == 4)
+                Console.WriteLine($"Twój wynik to: {dzielenie}");
+
+
+
+        }   }        } 
+        
